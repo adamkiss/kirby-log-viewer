d58aee74c1d61eb756065cb880800285bd55de06
Adam Kiss <iam@adamkiss.com>
2026-09-22 16:08:03 +0200

    fix null access to array

--- LogViewerArea.php
+++ LogViewerArea.php
@@ -2,22 +2,19 @@
 
 namespace AdamKiss\LogViewer;
 
-use DateTimeZone;
-use Kirby\Cms\App;
-use Kirby\Toolkit\A;
-use Kirby\Toolkit\Str;
-use Kirby\Filesystem\F;
-use Kirby\Toolkit\Date;
-use Kirby\Filesystem\Dir;
 use Highlight\Highlighter;
+use Kirby\Cms\App;
+use Kirby\Filesystem\Dir;
+use Kirby\Filesystem\F;
+use Kirby\Toolkit\A;
+use Kirby\Toolkit\Date;
+use Kirby\Toolkit\Str;
 
-class LogViewerArea
-{
+class LogViewerArea {
 	private static ?Highlighter $hl = null;
 	public const ID = 'adamkiss/kirby-log-viewer';
 
-	public static function blueprint(App $kirby): array
-	{
+	public static function blueprint(App $_): array {
 		return [
 			'id' => static::ID,
 			'breadcrumbLabel' => 'Kompletné logy',
@@ -39,10 +36,10 @@ class LogViewerArea
 		];
 	}
 
-	public static function view(?string $week = null): array
-	{
+	public static function view(?string $week = null): array {
 		$all = self::all();
 		$week ??= A::first($all)['name'];
+
 		$log = $all[$week];
 
 		$content = F::read(kirby()->root('logs') . '/' . $log['file']);
@@ -58,8 +55,7 @@ class LogViewerArea
 		];
 	}
 
-	public static function all(): array
-	{
+	public static function all(): array {
 		$files = Dir::files(kirby()->root('logs'), ['.', '..', '.gitkeep']);
 		$convert = function (string $log) {
 			$name = Str::split($log, '.')[0];
@@ -83,13 +79,12 @@ class LogViewerArea
 		return A::keyBy(A::map(array_reverse($files), $convert), 'name');
 	}
 
-	public static function parse(string $logs): array
-	{
+	public static function parse(string $logs): array {
 		$__ = A::map(
 			preg_split('/^(?=\[[\d\:\.\- \/]+\])/m', $logs),
-			fn($line) => static::parseLine($line)
+			fn ($line) => static::parseLine($line)
 		);
-		$__ = A::reduce($__, function($all, $el) {
+		$__ = A::reduce($__, function ($all, $el) {
 			if (empty($all)) {
 				return [$el];
 			}
@@ -105,8 +100,12 @@ class LogViewerArea
 		}, []);
 		$__ = array_reverse($__);
 
-		$grouped = A::reduce($__, function(array $all, $el) {
+		$grouped = A::reduce($__, function (array $all, $el) {
 			$key = empty($el['time']) ? null : Str::split($el['time'], ' ')[0];
+			if ($key === null) {
+				return $all;
+			}
+
 			if (A::has($all, $key)) {
 				$all[$key] = [$el];
 			} else {
@@ -119,8 +118,7 @@ class LogViewerArea
 
 	}
 
-	public  static function parseLine(string $line) : array
-	{
+	public static function parseLine(string $line): array {
 		$l = Str::match($line, '/^\[(?P<time>[\d\:\.\- \/]+)\]\s(?P<sev>\w+)\s(?P<content>[\s\S]*)$/s');
 		if (is_null($l)) {
 			return [
@@ -132,11 +130,11 @@ class LogViewerArea
 
 		$content = match(true) {
 			Str::startsWith($l['content'], '{') || Str::startsWith($l['content'], '[') =>
-				"<br><div hl>" . static::getHighlighter()->highlight('json', $l['content'])->value . '</div>',
+				'<br><div hl>' . static::getHighlighter()->highlight('json', $l['content'])->value . '</div>',
 			Str::startsWith($l['content'], '(') =>
-				preg_replace('/^\((.*?)\)(.*)$/', '<span class="tag">$1</span>$2',$l['content']),
+				preg_replace('/^\((.*?)\)(.*)$/', '<span class="tag">$1</span>$2', $l['content']),
 			Str::contains($l['content'], 'Stack trace:') =>
-				"<br>" . $l['content'],
+				'<br>' . $l['content'],
 			default =>
 				"<span>{$l['content']}</span>"
 		};
@@ -155,8 +153,7 @@ class LogViewerArea
 		];
 	}
 
-	public  static function getHighlighter() : Highlighter
-	{
+	public static function getHighlighter(): Highlighter {
 		if (is_null(static::$hl)) {
 			static::$hl = new Highlighter();
 		}
